0e4bb9c43b82d153b6182723dbbff27482984731
이민기 <mingy837@gmail.com>
2025-08-27 13:57:48 +0900

    최적 경로 백트래킹 순열 해결

--- src/main/java/org/swea/최적경로/Solution.java
+++ src/main/java/org/swea/최적경로/Solution.java
@@ -5,58 +5,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.Buffer;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
-import java.util.TreeSet;
+import java.util.*;
 
 // N+2 <= 12 이유가 edge 연결하면서 12! 사용 4억 ~= 4초 (20초 이내)
 public class Solution {
-    static class Edge implements Comparable<Edge>{
-        int from, to, weigh;
-        public Edge(int from, int to, int weigh){
-            this.from = from;
-            this.to = to;
-            this.weigh = weigh;
-        }
-        @Override
-        public int compareTo(Edge o) {
-            return Integer.compare(this.weigh, o.weigh);
-        }
-
-
-    }
-    private static int N, E;
-    private static TreeSet<Edge> edgeList;
-    private static int[] parent;
+    private static int result;
+    private static int N;
     private static Point[] point;
-    private static void make(){
-        for (int i = 0; i < N; i++){
-            parent[i] = i;
-        }
-    }
-    private static int find(int a){
-        if(a == parent[a]) return a;
-        return parent[a] = find(parent[a]); // path 압축
-    }
-    private static boolean union(int a, int b){
-        int aRoot = find(a);
-        int bRoot = find(b);
-        if(aRoot == bRoot) return false;
-
-        parent[bRoot] = aRoot;
-        return true;
-    }
-
+    private static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
         for(int t = 1; t <= T; t++){
+            result = Integer.MAX_VALUE;
             N = Integer.parseInt(br.readLine());
-
-            parent = new int[N+2];
-            edgeList = new TreeSet<>();
+            visited = new boolean[N];
             point = new Point[N+2];
             st = new StringTokenizer(br.readLine());
 
@@ -65,32 +29,41 @@ public class Solution {
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
                 if(i == 0) point[0] = new Point(x,y);
-                if(i == 1) point[N+1] = new Point(x,y);
+                else if(i == 1) point[N+1] = new Point(x,y);
                 else point[i-1] = new Point(x,y);
             }
 
-            for(int i = 0; i < N + 1; i++){
-                int x1 = point[i].x;
-                int y1 = point[i].y;
-                for(int j = i + 1; j < N + 2; j++){
-                    int x2 = point[j].x;
-                    int y2 = point[j].y;
-                    int from = i;
-                    int to = j;
-                    int weight = Math.abs(x1-x2) + Math.abs(y1-y2);
-                    edgeList.add(new Edge(from,to,weight));
-                }
-            }
-            make();
-            int result = 0;
-            int cnt = 0;
-            for(Edge edge : edgeList){
-                if(!union(edge.from, edge.to)) continue;
-                result += edge.weigh;
-                if(++cnt == N-1) break;
-            }
+            List<Integer> list = new ArrayList<>();
+            list.add(0);
+            permutaion(0, list);
             System.out.println("#" + t + " " + result);
+
+            }
+        }
+
+    private static void permutaion(int cnt, List<Integer> list){
+        if(cnt == N){
+            list.add(N+1);
+            int distance = 0;
+            for(int i = 0; i < list.size()-1; i++){
+                int x1 = point[list.get(i)].x;
+                int y1 = point[list.get(i)].y;
+                int x2 = point[list.get(i+1)].x;
+                int y2 = point[list.get(i+1)].y;
+                distance += Math.abs(x1 - x2) + Math.abs(y1 - y2);
+            }
+            list.remove(Integer.valueOf(N+1));
+            result = Math.min(result, distance);
+        }
+        for(int i = 1; i <= N; i++){
+            if(visited[i-1]) continue;
+            list.add(i);
+            visited[i-1] = true;
+            permutaion(cnt + 1, list);
+            list.remove(Integer.valueOf(i));
+            visited[i-1] = false;
         }
     }
-
 }
+
+
