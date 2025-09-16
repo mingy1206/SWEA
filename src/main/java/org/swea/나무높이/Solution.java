aba08628d0ac2a4050d78b10a96b8a073809aec2
이민기 <mingy837@gmail.com>
2025-09-17 01:15:00 +0900

    나무 높이 병합

--- src/main/java/org/swea/나무높이/Solution.java
+++ src/main/java/org/swea/나무높이/Solution.java
@@ -1,3 +1,5 @@
+package org.swea.나무높이;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
--- src/main/java/org/swea/원자소멸시뮬레이션/Solution.java
+++ src/main/java/org/swea/원자소멸시뮬레이션/Solution.java
@@ -1,13 +1,25 @@
 package org.swea.원자소멸시뮬레이션;
 
+<<<<<<< Updated upstream
 import java.awt.*;
 import java.io.*;
 import java.nio.Buffer;
 import java.util.*;
 import java.util.List;
+=======
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+>>>>>>> Stashed changes
 
-// 가로나 세로 거리 중 더 거리가 멀리 떨어져 있는 횟수 만큼 반복
 public class Solution {
+<<<<<<< Updated upstream
     private static final int[][] directions = {{0,1},{0,-1},{-1,0},{1,0}};//상하좌우 (dx,dy)로 저장
     static class Atom{
         int x, y, energy, dir;
@@ -15,12 +27,28 @@ public class Solution {
         Atom(int x, int y, int dir, int energy){
             this.x = x; this.y = y; this.dir = dir; this. energy = energy;
             isAlive = true;
+=======
+
+    // 수정: 배열/행렬 좌표계에 맞게 방향 재정의 {y변화량, x변화량}
+    // 0:상(y--), 1:하(y++), 2:좌(x--), 3:우(x++)
+    private static final int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    static class Atom {
+        int y, x, dir, energy;
+
+        public Atom(int y, int x, int dir, int energy) {
+            this.y = y;
+            this.x = x;
+            this.dir = dir;
+            this.energy = energy;
+>>>>>>> Stashed changes
         }
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
+<<<<<<< Updated upstream
         for(int t = 1; t<=T; t++){
             int totalEnergy = 0;
             int N = Integer.parseInt(br.readLine());
@@ -71,24 +99,63 @@ public class Solution {
             }
 
             System.out.println("#" + t + " " + totalEnergy);
+=======
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int N = Integer.parseInt(br.readLine());
+            List<Atom> atomList = new ArrayList<>();
+
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken()) * 2;
+                int y = Integer.parseInt(st.nextToken()) * 2;
+                int dir = Integer.parseInt(st.nextToken());
+                int energy = Integer.parseInt(st.nextToken());
+
+                // 수정: 불필요한 convertedDir 변수 및 로직 제거
+                atomList.add(new Atom(y, x, dir, energy));
+            }
+
+            long answer = 0;
+>>>>>>> Stashed changes
+
+            while (atomList.size() > 1) {
+                Map<Point, List<Atom>> map = new HashMap<>();
+
+<<<<<<< Updated upstream
 
 
+=======
+                for (Atom atom : atomList) {
+                    // 수정: atom.dir을 직접 사용
+                    int ny = atom.y + directions[atom.dir][0];
+                    int nx = atom.x + directions[atom.dir][1];
 
+                    if (ny < -2000 || ny > 2000 || nx < -2000 || nx > 2000) {
+                        continue;
+                    }
 
+                    map.computeIfAbsent(new Point(ny, nx), k -> new ArrayList<>()).add(new Atom(ny, nx, atom.dir, atom.energy));
+                }
 
+                List<Atom> nextAtomList = new ArrayList<>();
+
+                for (List<Atom> group : map.values()) {
+                    if (group.size() > 1) {
+                        for (Atom atom : group) {
+                            answer += atom.energy;
+                        }
+                    } else {
+                        nextAtomList.add(group.get(0));
+                    }
+                }
+
+                atomList = nextAtomList;
+            }
+>>>>>>> Stashed changes
+
+            System.out.println("#" + test_case + " " + answer);
         }
     }
 }
-/*
-2
-4
--1000 0 3 5
-1000 0 2 3
-0 1000 1 7
-0 -1000 0 9
-4
--1 1 3 3
-0 1 1 1
-0 0 2 2
--1 0 0 9
- */
+
