9ae0fdb5cb004fa3517b8b981e236d7fe4a63069
mingy1206 <mingy837@gmail.com>
2026-02-25 18:33:01 +0900

    맥주마시면서걸어가기 수정중

--- src/main/java/org/baekjoon/맥주_마시면서_걸어가기/Main.java
+++ src/main/java/org/baekjoon/맥주_마시면서_걸어가기/Main.java
@@ -3,33 +3,31 @@ package org.baekjoon.맥주_마시면서_걸어가기;
 import java.awt.*;
 import java.util.*;
 import java.io.*;
+import java.util.List;
+
 public class Main {
     static int[][] directions = {{0,1},{0,1},{-1,0},{0,-1}};
-    static int result;
-    static Point[] convenience;
-    static Point festival;
     static Point house;
+    static Point festival;
     static boolean[][] visited;
     static int n;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Map<Integer, Point> map = new HashMap<>();
         int T = Integer.parseInt(br.readLine());
-        int addValue = 32768;
         for(int t = 0; t < T; t++){
-            //visited = new boolean[32768*2][32768*2];
             n = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine());
-            convenience = new Point[n];
-            house = new Point(addValue+Integer.parseInt(st.nextToken()), addValue+Integer.parseInt(st.nextToken()));
+            house = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
 
             for(int i = 0; i < n; i++){
                 st = new StringTokenizer(br.readLine());
-                convenience[i] = new Point(addValue+Integer.parseInt(st.nextToken()), addValue+Integer.parseInt(st.nextToken()));
+                map.put(i, new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
             }
 
             st = new StringTokenizer(br.readLine());
-            festival = new Point(addValue+Integer.parseInt(st.nextToken()), addValue+Integer.parseInt(st.nextToken()));
-            result = 1;
+            festival = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            map.put(map.size(), festival);
 
 
             if(dfs(house,0, 20)) System.out.println("happy");
@@ -38,7 +36,6 @@ public class Main {
     }
     static boolean dfs(Point now, int cd, int beer){
         if(now.x == festival.x && now.y == festival.y){
-            result *= 0;
             return true;
         }
         for(int i = 0; i <n; i++){
