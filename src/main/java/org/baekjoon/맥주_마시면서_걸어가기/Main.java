d81bfdff28f698f98abb0f9359dbbaffb553b7e9
이민기 <mingy837@gmail.com>
2026-02-25 23:08:34 +0900

    맥주 마시면서 걸어가기

--- src/main/java/org/baekjoon/맥주_마시면서_걸어가기/Main.java
+++ src/main/java/org/baekjoon/맥주_마시면서_걸어가기/Main.java
@@ -9,14 +9,16 @@ public class Main {
     static int[][] directions = {{0,1},{0,1},{-1,0},{0,-1}};
     static Point house;
     static Point festival;
-    static boolean[][] visited;
+    static boolean[] visited;
     static int n;
+    static Map<Integer, Point> map;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Map<Integer, Point> map = new HashMap<>();
         int T = Integer.parseInt(br.readLine());
         for(int t = 0; t < T; t++){
+            map = new HashMap<>();
             n = Integer.parseInt(br.readLine());
+            visited = new boolean[n+1];
             StringTokenizer st = new StringTokenizer(br.readLine());
             house = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
 
@@ -24,38 +26,25 @@ public class Main {
                 st = new StringTokenizer(br.readLine());
                 map.put(i, new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
             }
-
             st = new StringTokenizer(br.readLine());
             festival = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
             map.put(map.size(), festival);
 
 
-            if(dfs(house,0, 20)) System.out.println("happy");
+            if(dfs(house)) System.out.println("happy");
             else System.out.println("sad");
         }
     }
-    static boolean dfs(Point now, int cd, int beer){
+    static boolean dfs(Point now){
         if(now.x == festival.x && now.y == festival.y){
             return true;
         }
-        for(int i = 0; i <n; i++){
-            if(now.x == convenience[i].x && now.y == convenience[i].y){
-                beer = 20;
-            }
-        }
-        if(cd > 50){
-            if(beer <= 0) return false;
-            else{
-                beer -= 1;
-                cd += 1;
-            }
-        }
-        for(int[] direction : directions){
-            int nx = now.x + direction[0];
-            int ny = now.y + direction[1];
-            if((nx < 0 || nx > 32767)||(ny < 0 || ny > 32767)) return false;
-            //if(visited[ny][nx]) return false;
-            if(dfs(new Point(nx,ny), cd+1, beer-1)) return true;
+        for(int i = 0; i < map.size(); i++){
+            if(visited[i]) continue;
+            Point next = map.get(i);
+            if(Math.abs(now.x-next.x) + Math.abs(now.y-next.y) > 1000) continue;
+            visited[i] = true;
+            if(dfs(next)) return true;
         }
         return false;
     }
